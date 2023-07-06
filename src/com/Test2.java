6b36a34554ebb532548eefc1e30c85dcbcbc7ccc
Yash Patil <yashpatil063@gmail.com>
2023-07-06 15:33:17 +0530

    Test2 committed and added to RemoteRepository

---
+++ src/com/Test2.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Test2 {
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		
+		if(hungry == "Yes")
+		{
+			System.out.println("Eat something---");
+		}
+		else {
+			System.out.println("Do work------");
+		}
+	}
+
+}
